1347bb6c0cefabff2b974f9d07d4332af3e971fd
harisriyoni3 <1214047@std.ulbi.ac.id>
2023-11-23 08:15:18 +0700

    get empid

--- jscroot/js/controller/controller_get_emp_id.js
+++ jscroot/js/controller/controller_get_emp_id.js
@@ -1,5 +1,42 @@
-// Function to make the API request with the token
-async function getEmployeeDetailsForLoggedInUser() {
+// Function to make the API request to get all employees
+async function getAllEmployees() {
+  const token = getTokenFromCookies('Login');
+
+  if (!token) {
+    alert("Token not found");
+    return;
+  }
+
+  const allEmployeesURL = 'https://asia-southeast2-gis-project-401902.cloudfunctions.net/get-all-employee';
+
+  const requestOptions = {
+    method: 'GET',
+    headers: {
+      'accept': 'application/json',
+      'Login': token,
+    },
+  };
+
+  try {
+    const response = await fetch(allEmployeesURL, requestOptions);
+    const data = await response.json();
+
+    if (data.status === 200 && data.data.length > 0) {
+      // Extract employeeid from the first employee in the list
+      const employeeId = data.data[0].employeeid;
+      // Call the function to post employee data using the extracted employeeid
+      postEmployeeData(employeeId);
+    } else {
+      // Handle error or empty employee list
+      alert('Error getting employee data');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+// Function to make the API request to post employee data
+async function postEmployeeData(employeeId) {
   const token = getTokenFromCookies('Login');
 
   if (!token) {
@@ -9,47 +46,37 @@ async function getEmployeeDetailsForLoggedInUser() {
 
   const oneEmployeeURL = 'https://asia-southeast2-gis-project-401902.cloudfunctions.net/one-employee';
 
-  // Set up headers with the token
-  const myHeaders = new Headers();
-  myHeaders.append('Login', token);
-
   const requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow',
-    employeeid: 'string'
+    method: 'POST',
+    headers: {
+      'accept': 'application/json',
+      'Login': token,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "employeeid": employeeId
+    })
   };
 
   try {
-    // Make a GET request to the one-employee endpoint without specifying the employee ID
     const response = await fetch(oneEmployeeURL, requestOptions);
     const data = await response.json();
 
     if (data.status === 200) {
-      // Update HTML element with the retrieved data
+      // Handle success
       displayEmployeeDetails(data.data);
     } else {
-      alert(data.message);
+      // Handle error
+      console.error('Error:', data.message);
     }
   } catch (error) {
     console.error('Error:', error);
   }
 }
 
-// Function to extract the token from cookies
-function getTokenFromCookies(cookieName) {
-  const cookies = document.cookie.split(';');
-  for (const cookie of cookies) {
-    const [name, value] = cookie.trim().split('=');
-    if (name === cookieName) {
-      return value;
-    }
-  }
-  return null;
-}
-
 // Function to display employee details in HTML
 function displayEmployeeDetails(employeeData) {
+  // Your existing displayEmployeeDetails function code goes here
   const employeeCardElement = document.getElementById('employeeCard');
 
   if (employeeData.employeeid !== "") {
@@ -60,12 +87,7 @@ function displayEmployeeDetails(employeeData) {
         <p><strong>Employee ID:</strong> ${employeeData.employeeid}</p>
         <p><strong>Name:</strong> ${employeeData.name}</p>
         <p><strong>Email:</strong> ${employeeData.email}</p>
-        <p><strong>Phone:</strong> ${employeeData.phone}</p>
-        <p><strong>Division:</strong> ${employeeData.division.divName}</p>
-        <p><strong>Username:</strong> ${employeeData.account.username}</p>
-        <p><strong>Password:</strong> ${employeeData.account.password}</p>
-        <p><strong>Basic Salary:</strong> ${employeeData.salary['basic-salary']}</p>
-        <p><strong>Honor Division:</strong> ${employeeData.salary['honor-division']}</p>
+        <!-- Add other details as needed -->
       </div>
     `;
   } else {
@@ -74,5 +96,5 @@ function displayEmployeeDetails(employeeData) {
   }
 }
 
-// Example usage: No need to provide an employee ID, it will be obtained from the token
-getEmployeeDetailsForLoggedInUser();
+// Call the function to get all employees and initiate the process
+getAllEmployees();
