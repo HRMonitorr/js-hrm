810a81b5057c3fa660ecfce637aeddaed0c0ce08
harisriyoni3 <1214047@std.ulbi.ac.id>
2023-11-28 23:54:25 +0700

    push

--- jscroot/js/controller/controller_employee.js
+++ jscroot/js/controller/controller_employee.js
@@ -1,4 +1,4 @@
-function getTokenFromCookies(cookieName) {
+const getTokenFromCookies = (cookieName) => {
   const cookies = document.cookie.split(';');
   for (const cookie of cookies) {
     const [name, value] = cookie.trim().split('=');
@@ -7,9 +7,9 @@ function getTokenFromCookies(cookieName) {
     }
   }
   return null;
-}
+};
 
-async function getAllEmployees() {
+const getAllEmployees = async () => {
   const token = getTokenFromCookies('Login');
 
   if (!token) {
@@ -40,9 +40,9 @@ async function getAllEmployees() {
   } catch (error) {
     console.error('Error:', error);
   }
-}
+};
 
-async function searchEmployee() {
+const searchEmployee = async () => {
   const employeeIdInput = document.getElementById('employeeIdInput').value;
 
   if (!employeeIdInput) {
@@ -81,13 +81,57 @@ async function searchEmployee() {
   } catch (error) {
     console.error('Error:', error);
   }
-}
+};
 
-function editEmployee(employeeId) {
-  window.location.href = `form.html?employeeid=${employeeId}`;
-}
+const deleteEmployee = async (employeeId) => {
+  const token = getTokenFromCookies('Login');
 
-function displayEmployeeData(employeeData, tableBodyId) {
+  if (!token) {
+    alert("Token login tidak ada");
+    return;
+  }
+
+  const targetURL = 'https://asia-southeast2-gis-project-401902.cloudfunctions.net/delete-employee';
+
+  const myHeaders = new Headers();
+  myHeaders.append('Login', token);
+  myHeaders.append('Content-Type', 'application/json');
+
+  const requestOptions = {
+    method: 'DELETE',
+    headers: myHeaders,
+    body: JSON.stringify({ employeeid: employeeId }),
+    redirect: 'follow'
+  };
+
+  try {
+    const response = await fetch(targetURL, requestOptions);
+    const data = await response.json();
+
+    if (data.status === 200) {
+      alert("Employee deleted successfully!");
+      getAllEmployees();
+    } else {
+      alert(data.message);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+const editEmployee = (employeeId) => {
+  window.location.href = `update.html?employeeid=${employeeId}`;
+};
+
+document.getElementById('searchButton').addEventListener('click', searchEmployee);
+
+const deleteEmployeeHandler = (employeeId) => {
+  if (confirm("Are you sure you want to delete this employee?")) {
+    deleteEmployee(employeeId);
+  }
+};
+
+const displayEmployeeData = (employeeData, tableBodyId) => {
   const employeeDataBody = document.getElementById(tableBodyId);
 
   employeeDataBody.innerHTML = '';
@@ -100,20 +144,22 @@ function displayEmployeeData(employeeData, tableBodyId) {
         <td>${emp.name}</td>
         <td>${emp.email}</td>
         <td>${emp.phone}</td>
-        <td>${emp.account['role']}</td>
+        <td>${emp.division['divName']}</td>
         <td>${emp.account['username']}</td>
+        <td>${emp.account['password']}</td>
         <td>${emp.salary['basic-salary']}</td>
         <td>${emp.salary['honor-division']}</td>
-        <td><a href="javascript:;">Edit</a></td>
+        <td>
+          <a href="javascript:;" onclick="editEmployee('${emp.employeeid}')">Edit</a>
+          <a href="javascript:;" onclick="deleteEmployeeHandler('${emp.employeeid}')">Delete</a>
+        </td>
       `;
-      newRow.querySelector('a').addEventListener('click', () => editEmployee(emp.employeeid));
       employeeDataBody.appendChild(newRow);
     });
   } else {
     employeeDataBody.innerHTML = `<tr><td colspan="9">No user data found.</td></tr>`;
   }
-}
+};
 
+// Initial fetch of all employees
 getAllEmployees();
-
-document.getElementById('searchButton').addEventListener('click', searchEmployee);
---
+++ jscroot/js/controller/controller_updateemp.js
@@ -0,0 +1,132 @@
+const getTokenFromCookies = (cookieName) => {
+  const cookies = document.cookie.split(';');
+  for (const cookie of cookies) {
+    const [name, value] = cookie.trim().split('=');
+    if (name === cookieName) {
+      return value;
+    }
+  }
+  return null;
+};
+
+const searchEmployeeById = async (employeeId) => {
+  const token = getTokenFromCookies('Login');
+
+  if (!token) {
+    alert("Anda Belum Login");
+    return;
+  }
+
+  const targetURL = 'https://asia-southeast2-gis-project-401902.cloudfunctions.net/one-employee';
+
+  const myHeaders = new Headers();
+  myHeaders.append('Login', token);
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: JSON.stringify({ employeeid: employeeId }),
+    redirect: 'follow',
+  };
+
+  try {
+    const response = await fetch(targetURL, requestOptions);
+    const data = await response.json();
+
+    if (data.status === 200) {
+      populateForm(data.data);
+    } else {
+      alert(data.message);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+const populateForm = (employeeData) => {
+  const setValue = (id, value) => {
+    document.getElementById(id).value = value;
+  };
+
+  setValue('employeeIdInput', employeeData.employeeid);
+  setValue('nameInput', employeeData.name);
+  setValue('emailInput', employeeData.email);
+  setValue('phoneInput', employeeData.phone);
+  setValue('divisionInput', employeeData.division.divName);
+  setValue('usernameInput', employeeData.account.username);
+  setValue('passwordInput', employeeData.account.password);
+  setValue('basicSalaryInput', employeeData.salary['basic-salary']);
+  setValue('honorDivisionInput', employeeData.salary['honor-division']);
+
+  document.getElementById('employeeForm').style.display = 'block';
+};
+
+const updateEmployee = async (event) => {
+  event.preventDefault();
+
+  const employeeId = document.getElementById('employeeIdInput').value;
+  const token = getTokenFromCookies('Login');
+
+  if (!token) {
+    alert("Anda Belum Login");
+    return;
+  }
+
+  const targetURL = 'https://asia-southeast2-gis-project-401902.cloudfunctions.net/updatedataemployee';
+
+  const myHeaders = new Headers();
+  myHeaders.append('Login', token);
+  myHeaders.append('Content-Type', 'application/json');
+
+  const basicSalaryInput = document.getElementById('basicSalaryInput').value;
+  const honorDivisionInput = document.getElementById('honorDivisionInput').value;
+
+  if (isNaN(basicSalaryInput) || isNaN(honorDivisionInput)) {
+    alert('Please enter valid numeric values for salary.');
+    return;
+  }
+
+  const requestOptions = {
+    method: 'PUT',
+    headers: myHeaders,
+    body: JSON.stringify({
+      employeeid: employeeId,
+      name: document.getElementById('nameInput').value,
+      email: document.getElementById('emailInput').value,
+      phone: document.getElementById('phoneInput').value,
+      division: { divName: document.getElementById('divisionInput').value },
+      account: {
+        username: document.getElementById('usernameInput').value,
+        password: document.getElementById('passwordInput').value,
+      },
+      salary: {
+        'basic-salary': parseInt(basicSalaryInput),
+        'honor-division': parseInt(honorDivisionInput),
+      },
+    }),
+    redirect: 'follow',
+  };
+
+  try {
+    const response = await fetch(targetURL, requestOptions);
+    const data = await response.json();
+
+    if (data.status === 200) {
+      alert("Employee updated successfully!");
+    } else {
+      alert(data.message);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+const employeeIdFromURL = new URLSearchParams(window.location.search).get('employeeid');
+if (employeeIdFromURL) {
+  document.getElementById('employeeIdInput').value = employeeIdFromURL;
+  searchEmployeeById(employeeIdFromURL);
+}
+
+document.getElementById('employeeForm').style.display = 'block';
+
+document.getElementById('employeeForm').addEventListener('submit', updateEmployee);
