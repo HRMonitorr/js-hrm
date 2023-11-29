cf5d198383af96cf5fc56ffa2329f77a97f30e97
harisriyoni3 <1214047@std.ulbi.ac.id>
2023-11-29 17:37:20 +0700

    sudah fix CRUD ganti tampilan aja

--- jscroot/js/controller/controller_employee.js
+++ jscroot/js/controller/controller_employee.js
@@ -140,19 +140,56 @@ const displayEmployeeData = (employeeData, tableBodyId) => {
     employeeData.forEach(emp => {
       const newRow = document.createElement('tr');
       newRow.innerHTML = `
-        <td>${emp.employeeid}</td>
-        <td>${emp.name}</td>
-        <td>${emp.email}</td>
-        <td>${emp.phone}</td>
-        <td>${emp.division['divName']}</td>
-        <td>${emp.account['username']}</td>
-        <td>${emp.account['password']}</td>
-        <td>${emp.salary['basic-salary']}</td>
-        <td>${emp.salary['honor-division']}</td>
-        <td>
-          <a href="javascript:;" onclick="editEmployee('${emp.employeeid}')">Edit</a>
-          <a href="javascript:;" onclick="deleteEmployeeHandler('${emp.employeeid}')">Delete</a>
-        </td>
+      <td class="px-4 py-3">
+      <div class="flex items-center text-sm">
+        <div>
+          <p class="font-semibold">${emp.employeeid}</p>
+        </div>
+      </div>
+    </td>
+    <td class="px-4 py-3 text-sm">
+      <p class="font-semibold">>${emp.name}</p>
+    </td>
+    <td class="px-4 py-3 text-sm">
+      <p class="font-semibold">${emp.email}</p>
+    </td>
+    <td class="px-4 py-3 text-sm">
+      ${emp.phone}
+    </td>
+    <td class="px-4 py-3 text-xs">
+      <span
+        class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">
+        ${emp.division['divName']}
+      </span>
+    </td>
+    <td class="px-4 py-3 text-sm">
+      ${emp.salary['basic-salary']}
+    </td>
+    <td class="px-4 py-3 text-sm">
+      ${emp.salary['honor-division']}
+    </td>
+    <td class="px-4 py-3">
+      <div class="flex items-center space-x-4 text-sm">
+        <a href="javascript:;"
+          class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+          aria-label="Edit" onclick="editEmployee('${emp.employeeid}')">
+          <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+            <path
+              d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z">
+            </path>
+          </svg>
+        </a>
+        <a href="javascript:;"
+          class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+          aria-label="Delete">
+          <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+            <path fill-rule="evenodd"
+              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+              clip-rule="evenodd" onclick="editEmployee('${emp.employeeid}')"></path>
+          </svg>
+        </a>
+      </div>
+    </td>
       `;
       employeeDataBody.appendChild(newRow);
     });
