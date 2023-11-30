9bfc7334f8497a271ab4b64ad0ddb3eec153b670
harisriyoni3 <1214047@std.ulbi.ac.id>
2023-11-30 23:53:07 +0700

    update

--- jscroot/js/controller/controller_logout.js
+++ jscroot/js/controller/controller_logout.js
@@ -1,10 +1,8 @@
 import { deleteCookie } from 'https://jscroot.github.io/cookie/croot.js';
 
-function logout() {
-
+const logout = () => {
   deleteCookie('Login');
   window.location.href = 'https://hrmonitor.advocata.me/dashboard/public/pages/login.html';
-}
-
+};
 
 document.getElementById('logoutButton').addEventListener('click', logout);
