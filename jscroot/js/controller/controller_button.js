7b7feb75a61cb46fbbc31c762aca6a4ea1a3f1b5
harisriyoni3 <1214047@std.ulbi.ac.id>
2023-11-30 20:54:31 +0700

    button

--- jscroot/js/controller/controller_button.js
+++ jscroot/js/controller/controller_button.js
@@ -1,32 +1,32 @@
-function checkLoginStatus() {
-  const token = getTokenFromCookies('Login'); // Ganti 'user_token' dengan nama cookie yang sesuai
-
-  if (token) {
-      // Jika ada token, pengguna sudah login. Tampilkan tombol Logout.
-      document.getElementById('logoutButton').style.display = 'block';
-      // Sembunyikan tombol Sign In dan Sign Up.
-      document.getElementById('signInButton').style.display = 'none';
-      document.getElementById('signUpButton').style.display = 'none';
-  } else {
-      // Jika tidak ada token, pengguna belum login. Tampilkan tombol Sign In dan Sign Up.
-      document.getElementById('signInButton').style.display = 'block';
-      document.getElementById('signUpButton').style.display = 'block';
-      // Sembunyikan tombol Logout.
-      document.getElementById('logoutButton').style.display = 'none';
-  }
-}
-
 // Function to extract the token from cookies
 function getTokenFromCookies(cookieName) {
   const cookies = document.cookie.split(';');
   for (const cookie of cookies) {
-      const [name, value] = cookie.trim().split('=');
-      if (name === cookieName) {
-          return value;
-      }
+    const [name, value] = cookie.trim().split('=');
+    if (name === cookieName) {
+      return value;
+    }
   }
   return null;
+}
+// Function to check login status and toggle visibility
+function checkLoginStatus() {
+  const token = getTokenFromCookies('Login'); // Ganti 'user_token' dengan nama cookie yang sesuai
+
+  const pagesMenu = document.getElementById('pagesMenu');
+  const loginPage = document.getElementById('loginPage');
+  const registerPage = document.getElementById('registerPage');
+
+  if (token) {
+    // Jika ada token, pengguna sudah login. Sembunyikan halaman login dan register.
+    pagesMenu.style.display = 'none';
+  } else {
+    // Jika tidak ada token, pengguna belum login. Tampilkan halaman login dan register.
+    pagesMenu.style.display = 'block';
+  }
 }
 
 // Panggil fungsi untuk memeriksa status login saat halaman dimuat
 checkLoginStatus();
+
+export { getTokenFromCookies, checkLoginStatus };
