db7ff3cd117d50905751fd7ddb9daded78693fbf
harisriyoni3 <1214047@std.ulbi.ac.id>
2023-11-30 21:57:25 +0700

    logout function

--- jscroot/js/controller/controller_logout.js
+++ jscroot/js/controller/controller_logout.js
@@ -1,5 +1,12 @@
-  import { deleteCookie } from "https://jscroot.github.io/cookie/croot.js";
+import { deleteCookie } from "https://jscroot.github.io/cookie/croot.js";
 
 function logout() {
   deleteCookie("Login");
 }
+
+document.getElementById("logoutContainer").addEventListener("click", function (event) {
+  // Check if the clicked element is the logout link
+  if (event.target.matches('a[href="#"]')) {
+    logout();
+  }
+});
