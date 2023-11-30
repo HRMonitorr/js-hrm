b5360785124665ab7e321ca9866b79c84869017f
harisriyoni3 <1214047@std.ulbi.ac.id>
2023-11-30 21:14:15 +0700

    logout function

--- jscroot/js/controller/controller_logout.js
+++ jscroot/js/controller/controller_logout.js
@@ -1,18 +1,10 @@
-// Import the deleteCookie function
 import { deleteCookie } from 'https://jscroot.github.io/cookie/croot.js';
 
 function logout() {
+
   deleteCookie('Login');
-  
   window.location.href = 'https://hrmonitor.advocata.me/dashboard/public/pages/login.html';
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  const logoutButton = document.getElementById('logoutButton');
-  
-  if (logoutButton) {
-    logoutButton.addEventListener('click', logout);
-  } else {
-    console.error("Element with ID 'logoutButton' not found");
-  }
-});
+
+document.getElementById('logoutButton').addEventListener('click', logout);
