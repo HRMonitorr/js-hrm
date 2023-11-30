ac26836d7c433604241b2f2bbe70afec96e3ef52
harisriyoni3 <1214047@std.ulbi.ac.id>
2023-11-30 23:01:53 +0700

    fic

--- jscroot/js/controller/controller_logout.js
+++ jscroot/js/controller/controller_logout.js
@@ -1,6 +1,13 @@
+// LogoutFunction.js
 import { deleteCookie } from "./cookies.js";
 
 export default function Logout() {
     deleteCookie();
     window.location.href = "pages/login.html";
 }
+
+// Add an event listener to the logout link
+document.getElementById('logoutLink').addEventListener('click', function (event) {
+    event.preventDefault(); // Prevents the default link behavior (navigating to the href)
+    Logout(); // Call the Logout function when the link is clicked
+});
