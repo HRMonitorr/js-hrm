9b6e0d39cfe74ac680ed1cb4897e0001308f5d8c
harisriyoni3 <1214047@std.ulbi.ac.id>
2023-11-30 23:29:09 +0700

    fic

--- jscroot/js/controller/controller_logout.js
+++ jscroot/js/controller/controller_logout.js
@@ -1,6 +1,6 @@
 import { deleteCookie } from "https://jscroot.github.io/cookie/croot.js";
 
-export default function Logout() {
-    deleteCookie();
-    window.location.href = "pages/login.html";
+function logout() {
+  deleteCookie();
+  window.location.href = "pages/login.html";
 }
